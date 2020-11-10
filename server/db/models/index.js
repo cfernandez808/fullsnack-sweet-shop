bde52eedc59bbe3d1e0cf286e4b2f50b13738179
Christian Fernandez <tdc027@protonmail.com>
2020-11-10 10:09:59 -0800

    Made Cart & Order schemas. Made all assocations for eager loading

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,18 @@
 const User = require('./user')
+const Order = require('./order')
+const Cart = require('./cart')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// Cart.hasMany(Product)
+// Product.belongsTo(Cart)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Cart.belongsTo(User)
+User.hasOne(Cart)
+
+Order.belongsTo(User)
+User.hasMany(Order)
+
 module.exports = {
-  User
+  User,
+  Order,
+  Cart
 }
