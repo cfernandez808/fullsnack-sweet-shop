8df0da9b948460dc1bcccb60e99561dfc1a5c2b2
Christian Fernandez <tdc027@protonmail.com>
2020-11-10 09:20:32 -0800

    Added User Schema & changed all tests to work with new schema.

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,27 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
   },
   password: {
     type: Sequelize.STRING,
@@ -26,6 +43,12 @@ const User = db.define('user', {
   },
   googleId: {
     type: Sequelize.STRING
+  },
+  shippingAddress: {
+    type: Sequelize.TEXT
+  },
+  billingAddress: {
+    type: Sequelize.TEXT
   }
 })
 
