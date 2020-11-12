5d749f4f53c5830de3fa29a9f4a3a0557358638b
Brandie Burditt <brandieburditt@gmail.com>
2020-11-12 12:18:56 -0500

    working add to cart button on single page view with redux store

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,18 +1,24 @@
 import axios from 'axios'
 
 const GET_CART = 'GET_CART'
+// const ADD_TO_CART = 'ADD_TO_CART'
 
 const defaultCart = {}
 
-const getCart = cart => ({
+const getCart = (cart) => ({
   type: GET_CART,
-  cart
+  cart,
 })
 
-export const getCartThunk = id => async dispatch => {
+// const addToCart = (candy) => ({
+//   type: ADD_TO_CART,
+//   candy,
+// })
+
+export const getCartThunk = (id) => async (dispatch) => {
   try {
     let {data} = await axios.get(`/api/cart/${id}`)
-    data = data.carts.map(candy => {
+    data = data.carts.map((candy) => {
       candy.candies[0].quantity = candy.quantity
       candy.candies[0].id = candy.id
       return candy.candies[0]
@@ -23,7 +29,22 @@ export const getCartThunk = id => async dispatch => {
   }
 }
 
-export default function(state = defaultCart, action) {
+export const addCandyToCart = (userId, candyObj) => async (dispatch) => {
+  try {
+    await axios.post(`/api/cart/${userId}`, candyObj)
+    let {data} = await axios.get(`/api/cart/${userId}`)
+    data = data.carts.map((candy) => {
+      candy.candies[0].quantity = candy.quantity
+      candy.candies[0].id = candy.id
+      return candy.candies[0]
+    })
+    dispatch(getCart(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default function (state = defaultCart, action) {
   switch (action.type) {
     case GET_CART:
       return action.cart
