58fbe0248a200b87e0aa924f95f9491db8b7524a
Christian Fernandez <tdc027@protonmail.com>
2020-11-12 09:31:57 -0800

    feature: removes users and candies in front and back end. And added products to admin dash

--- client/store/candy.js
+++ client/store/candy.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const GET_CANDY = 'GET_CANDY'
-// const REMOVE_CANDY = 'REMOVE_CANDY'
+const REMOVE_CANDY = 'REMOVE_CANDY'
 
 const defaultCandy = []
 
 const getCandy = candy => ({type: GET_CANDY, candy})
-// const removeCandy = () => ({type: REMOVE_CANDY})
+const removeCandy = id => ({type: REMOVE_CANDY, id})
 
 export const getCandyThunk = () => async dispatch => {
   try {
@@ -17,10 +17,23 @@ export const getCandyThunk = () => async dispatch => {
   }
 }
 
+export const removeCandyThunk = id => async dispatch => {
+  try {
+    await axios.delete(`/api/candy/${id}`)
+    dispatch(removeCandy(id))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 export default function(state = defaultCandy, action) {
   switch (action.type) {
     case GET_CANDY:
       return action.candy
+    case REMOVE_CANDY:
+      // eslint-disable-next-line no-case-declarations
+      const newState = state.filter(candy => candy.id !== action.id)
+      return newState
     default:
       return state
   }
