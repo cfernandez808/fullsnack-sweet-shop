010457ca725c0bb4d639113d04e5906f58f288b7
Chris Smith <c.a.smith1987@gmail.com>
2020-11-13 14:31:09 -0600

    Fix: Missed one route in prior commit.

--- server/api/users.js
+++ server/api/users.js
@@ -4,13 +4,15 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    if (req.user && req.user.dataValues && req.user.dataValues.admin) {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email'],
+      })
+      res.json(users)
+    } else res.send('Admin access only.')
   } catch (err) {
     next(err)
   }
